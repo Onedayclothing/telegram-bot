5a2dac9be596eaf9c046351f8118a3cfe8deb5ee
finnmyidol-hub <finnmyidol@gmail.com>
2026-09-02 13:57:44 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { Telegraf } = require('telegraf');
 const express = require('express');
+const crypto = require('crypto');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 const app = express();
@@ -7,8 +8,14 @@ const app = express();
 app.get('/', (req, res) => res.send('Bot is running!'));
 app.listen(process.env.PORT || 3000);
 
+// Command: /start
 bot.start((ctx) => ctx.reply('ជំរាបសួរ! Bot ដំណើរការហើយ។'));
-bot.help((ctx) => ctx.reply('ផ្ញើសារមកកាន់ Bot ដើម្បីសាកល្បង។'));
+
+// Command: /genkey
+bot.command('genkey', (ctx) => {
+  const randomKey = '0D-' + crypto.randomBytes(6).toString('hex').toUpperCase();
+  ctx.reply(`🔑 KEY ថ្មី (30d):\n/activate ${randomKey}\n\nផ្ញើ command ខាងលើទៅអ្នកប្រើ ដើម្បីដំណើរការ។`);
+});
 
 bot.launch();
 console.log('Bot started successfully!');
